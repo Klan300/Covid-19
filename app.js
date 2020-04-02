23ed704087a3fdca31867edb68db60e3855ddb42
Klan300 <nathun.thun@hotmail.com>
2020-04-02 22:54:01 +0700

    fix bug

--- app.js
+++ app.js
@@ -4,22 +4,22 @@ const fetch = require('node-fetch');
 const port = 3000
 var covid ;
 
-fetch("https://covid19.th-stat.com/api/open/timeline")
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
-    covid = data;
-  })
+// fetch("https://covid19.th-stat.com/api/open/timeline")
+//   .then(response => {
+//     return response.json();
+//   })
+//   .then(data => {
+//     covid = data;
+//   })
 
-app.get('/',function(req,res){
-    res.send(covid);
-})
+// app.get('/',function(req,res){
+//     res.send(covid);
+// })
 
-app.get('/today', function(req,res){
-    var today = covid.Data.slice(-1)[0];
-    res.send(today);
-})
+// app.get('/today', function(req,res){
+//     var today = covid.Data.slice(-1)[0];
+//     res.send(today);
+// })
 
 app.post('/webhook', (req, res) => res.sendStatus(200))
 
